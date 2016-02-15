a7db56951529f3a48a6fad16bcb25730cbd28be8
dodik gaghan <dodik@montazze.com>
2016-02-15 12:01:22 +0700

    update main file

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 /* 
-* @Author: Dodik Gaghan
-* @Date:   2016-02-15 11:52:33
-* @Last Modified by:   Dodik Gaghan
-* @Last Modified time: 2016-02-15 11:52:33
-*/
+ * @Author: Dodik Gaghan
+ * @Date:   2016-02-15 11:52:33
+ * @Last Modified by:   Dodik Gaghan
+ * @Last Modified time: 2016-02-15 12:01:12
+ */
 
 'use strict';
+
+var Script = require('deployd/lib/script');
+var _run = Script.prototype.run;
+Script.prototype.run = function(ctx, domain, fn) {
+	if (typeof domain === 'object') {
+		domain.url = ctx.url;
+		domain.parts = parts;
+		domain.query = ctx.query;
+		domain.body = ctx.body;
+		domain.require = function(module) {
+			return require(module);
+		}
+		domain.setResponse = function(val) {
+			result = val;
+		}
+	}
+	_run.call(this, ctx, domain, fn);
+}
