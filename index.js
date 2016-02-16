e9c579085e9b2c19e4cc3b6d776197a19949eb6c
dodik gaghan <dodik@montazze.com>
2016-02-16 14:57:42 +0700

    remove some dependencies

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
  * @Author: Dodik Gaghan
  * @Date:   2016-02-15 11:52:33
  * @Last Modified by:   Dodik Gaghan
- * @Last Modified time: 2016-02-15 18:02:59
+ * @Last Modified time: 2016-02-16 14:57:10
  */
 
 'use strict';
 
 var Script = require('deployd/lib/script');
 var _ = require('underscore');
-var randomstring = require("randomstring");
+var appRoot = require('app-root-path');
 
 var _run = Script.prototype.run;
 Script.prototype.run = function(ctx, domain, fn) {
@@ -18,29 +18,25 @@ Script.prototype.run = function(ctx, domain, fn) {
 			return p;
 		});
 
+		domain.appRoot = appRoot;
 		domain.url = ctx.url;
 		domain.parts = parts;
 		domain.query = ctx.query;
 		domain.body = ctx.body;
+		domain.reqlib = appRoot.require;
+		// domain.helper = appRoot.require('/lib/helper.js')(ctx, domain);
+
+		domain.require = function(module) {
+			return require(module);
+		}
 		domain.validate = function(object) {
 			var result = _.every(_.values(object), function(item) {
 				return item;
 			});
-			var err = {
-				message: 'Validation Error',
+			if (!result) return ctx.done({
 				errors: object
-			};
-			if (!result) {
-				ctx.done(err);
-				throw err;
-			}
+			});
 			return true;
-		}
-		domain.require = function(module) {
-			return require(module);
-		}
-		domain.randomstring = function(options) {
-			return randomstring.generate(options);
 		}
 	}
 	_run.call(this, ctx, domain, fn);
